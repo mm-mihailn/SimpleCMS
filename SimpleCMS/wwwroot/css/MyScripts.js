088644cae0142f372fb654fff2d8d588c0b8e6fd
Stefcho18 <stefchoplamenovk@gmail.com>
2024-01-09 11:39:55 +0200

    repair ID

--- SimpleCMS/wwwroot/css/MyScripts.js
+++ SimpleCMS/wwwroot/css/MyScripts.js
@@ -1,17 +1,17 @@
+﻿
+let mybutton = document.getElementById("myBtn");
+      
+window.onscroll = function() {scrollFunction()};
 
-      let mybutton = document.getElementById("myBtn");
-      
-      window.onscroll = function() {scrollFunction()};
-      
-      function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          mybutton.style.display = "block";
-        } else {
-          mybutton.style.display = "none";
-        }
-      }
-      
-      function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
