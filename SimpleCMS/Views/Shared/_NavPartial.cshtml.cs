c11ca9e62d026c2c47bc20414bd46a1ae32bb64b
Stefcho18 <stefchoplamenovk@gmail.com>
2024-02-12 22:11:07 +0200

    Start

--- SimpleCMS/Views/Shared/_NavPartial.cshtml.cs
+++ SimpleCMS/Views/Shared/_NavPartial.cshtml.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using SimpleCMS.Admin.Models;
 namespace SimpleCMS.Admin.Views.Shared
 {
     public class _NavPartialModel : PageModel
     {
+        public List<MenuItemsViewModel> DynamicMenuItems = new List<MenuItemsViewModel>();
         public void OnGet()
         {
+            DynamicMenuItems.Add(new MenuItemsViewModel { Link = "#", Title = "Училище" });
         }
     }
 }
