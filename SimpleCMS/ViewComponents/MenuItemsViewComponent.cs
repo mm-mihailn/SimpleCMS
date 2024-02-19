7d753534533357a19f0b6618774c404132753f67
Stefcho18 <stefchoplamenovk@gmail.com>
2024-02-19 12:48:13 +0200

    MakeingComponent

---
+++ SimpleCMS/ViewComponents/MenuItemsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCMS.Business.Services.Interfaces;
+using System.Threading.Tasks;
+namespace SimpleCMS.Web.ViewComponents
+{
+    public class MenuItemsViewComponent: ViewComponent
+    {
+        private IMenuItemsService _menuItemsService;
+        public MenuItemsViewComponent(IMenuItemsService menuItemsService)
+        {
+            _menuItemsService = menuItemsService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var menuItem = await _menuItemsService.GetMenuItemsAsync();
+            return View(menuItem);
+        }
+    }
+}
