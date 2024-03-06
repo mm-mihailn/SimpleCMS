4b2fba3c76b62eb0a2981cfe24ee861c02e02e18
Stefcho18 <stefchoplamenovk@gmail.com>
2024-03-06 21:37:07 +0200

    AddingWelcomeSection

--- SimpleCMS/ViewComponents/MenuItemsViewComponent.cs
+++ SimpleCMS/ViewComponents/MenuItemsViewComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SimpleCMS.Business.Services;
 using SimpleCMS.Business.Services.Interfaces;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -13,10 +15,13 @@ namespace SimpleCMS.Web.ViewComponents
         }
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            //var menuItems = await _menuItemsService.GetMenuItemsAsync();
-            //var model = menuItems.Where(p=>p.ParentId==null).Select(p => p.Title);
-            ////return View(model.Any()?model.ToList():new List<string>());
-            List<string> list = new List<string>() { "училище", "прием", "за родителя", "за ученика", "контакти", "галерия", "профил на купувача" } ;
+            //    var menuItems = await _menuItemsService.GetMenuItemsAsync();
+            //    return View(menuItems);
+            //    //var model = menuItems.Where(p => p.ParentId == null).Select(p => p.Title);
+            //    //return View(model.Any() ? model.ToList() : new List<string;
+
+
+            List<string> list = new List<string>() { "училище", "прием", "за родителя", "за ученика", "контакти", "галерия", "профил на купувача" };
             return View(list);
 
 
