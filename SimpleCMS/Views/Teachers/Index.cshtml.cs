9d63847de1939aa3adf5f2b498f8ab45a6246af6
Stefcho18 <stefchoplamenovk@gmail.com>
2024-03-13 21:46:07 +0200

    AddingTeazhersPage

---
+++ SimpleCMS/Views/Teachers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleCMS.Web.Views.Teachers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
