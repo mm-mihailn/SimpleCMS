886bca9567a6c14caed9361665c675e90e15d16d
Stefcho18 <stefchoplamenovk@gmail.com>
2024-03-21 23:32:47 +0200

    Test

---
+++ SimpleCMS/Views/Specialities/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SimpleCMS.Web.Views.Specialities
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
