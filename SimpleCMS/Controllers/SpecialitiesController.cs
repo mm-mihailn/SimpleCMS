ee50027c5e4ae36f86477a134f0a138b1678afa1
Stefcho18 <stefchoplamenovk@gmail.com>
2024-03-26 13:55:30 +0200

    AddingController

---
+++ SimpleCMS/Controllers/SpecialitiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleCMS.Web.Controllers
+{
+    public class SpecialitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
